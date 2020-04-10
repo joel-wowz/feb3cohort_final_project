11db72b717b43907fa0bde8e57aa606b63b44951
joel-wowz <josephlafluer@hotmail.com>
2020-04-10 05:12:29 +0000

    added state change on button click

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 export default function App(props) {
+  const [ state, setState ] = useState({
+    message: 'whats up',
+  });
+  const fetchData = () => {
+    axios
+      .get('/api/data') // You can simply make your requests to "/api/whatever you want"
+      .then((response) => {
+        // handle success
+        console.log(response.data); // The entire response from the Rails API
+
+        console.log(response.data.message); // Just the message
+        setState({
+          message: response.data.message,
+        });
+      });
+  };
+  console.log(state.message);
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <button>Fetch Data</button>
+      <h1> {state.message} </h1>
+      <button onClick={fetchData}>Fetch Data</button>
+      <Test />
     </div>
   );
 }
