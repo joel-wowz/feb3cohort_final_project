45279973c8ddd3a8321f06adaedac70f14e0bb4d
joelwowz <josephlafleur@hotmail.com>
2020-04-16 12:29:34 -0600

    removed hamburg

--- client/src/SearchAppBar.js
+++ client/src/SearchAppBar.js
@@ -7,7 +7,7 @@ import InputBase from '@material-ui/core/InputBase';
 import { fade, makeStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
-
+//updated and removed hambuger
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
