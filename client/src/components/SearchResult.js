c882df9c1bae4cf9e923c37083f64cc2a59e2a37
joelwowz <josephlafleur@hotmail.com>
2020-04-15 19:15:21 -0600

    finished searchresults

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const SearchResult = (props) => {
   const { result } = props;
-
   return (
     <div className="search-result">
       <h2>{result.name}</h2>
       <p>{result.volume}</p>
+      <h3>{result.matches.join(', ')}</h3>
     </div>
   );
 };
--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SearchResult from './SearchResult';
 
 const SearchResults = (props) => {
-  return props.results.map((result) => <SearchResult key={result.id} result={result} />);
+  return props.results.map((result) => <SearchResult key={result.id} result={result} {...result} />);
 };
 
 export default SearchResults;
