02e3941a818f703dfdef66c87cd25009d6a43bf0
joelwowz <josephlafleur@hotmail.com>
2020-04-20 21:36:58 -0600

    smaller

--- client/src/App.js
+++ client/src/App.js
@@ -65,7 +65,7 @@ export default function App() {
       <CssBaseline />
       <SearchAppBar onClick={filterResults} />
       <CheckSearch />
-      {<SnackCondition />}
+      <SnackCondition />
     </ThemeProvider>
   );
 }
